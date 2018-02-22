912c2dd8952bf9d474e10cc978e7dee77fabb3a3
Anwar Ali <anwar@Anwars-MacBook-Pro.local>
2018-02-22 22:47:14 +0545

    listing categories

--- admin/modules/categories/index.php
+++ admin/modules/categories/index.php
@@ -2,7 +2,27 @@
 include('../../includes/session.php');
 include('../../includes/constants.php');
 include('../../includes/header.php');
-
+include('../../includes/connection.php');
+$link = connect();
+$result = execute($link,"SELECT * FROM categories");
 ?>
 <h1>Category Manager</h1>
+<?php
+if(num_rows($result) > 0)
+{
+	?>
+	<table width="100%;" border="1">
+		<tr><td>ID</td><td>Name</td></tr>
+		<?php
+		while($cat = fetch($result))
+		{
+			?>
+			<tr><td><?php echo $cat['id'];?></td><td><?php echo $cat['name'];?></td></tr>
+			<?php
+		}
+		?>
+	</table>
+	<?php
+}
+?>
 <?php include('../../includes/footer.php');?>
